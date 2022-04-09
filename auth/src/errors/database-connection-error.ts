cd651a57b83d6c8b47826c9090925b3a3ad74cd2
rodriggj <gabriel.rodriguez.ctr@gmail.com>
2022-04-09 12:27:48 -0600

    added error handler logic

---
+++ auth/src/errors/database-connection-error.ts
@@ -0,0 +1,11 @@
+export class DatabaseConnectionError extends Error {
+    reason = 'Error connecting to database.'
+    constructor() {
+        super()
+
+        // Only because we are extending a built in  base class 
+        Object.setPrototypeOf(this, DatabaseConnectionError.prototype)
+
+
+    }
+}
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response }from 'express'
 import { body, validationResult } from 'express-validator'
+import { RequestValidationError } from '../errors/request-validation-error'
+import { DatabaseConnectionError } from '../errors/database-connection-error'
 
 const router = express.Router()
 
@@ -15,17 +17,19 @@ router.post(
       .withMessage('Password must be between 4 and 20 characters')
 ], 
 (req: Request, res: Response) => {
-    const errors = validationResult(req)
+        const errors = validationResult(req)
 
-    if(!errors.isEmpty()) {
-        return res.status(400).send(errors.array())
+        if(!errors.isEmpty()) {
+            throw new RequestValidationError(errors.array())
+        }
+
+        const { email, password } = req.body
+
+        console.log('Creating a user...')
+        throw new DatabaseConnectionError()
+
+        res.send({})
     }
-
-    const { email, password } = req.body
-
-    console.log('Creating a user...')
-
-    res.send({})
-}
 )
+
 export { router as signupRouter}
